a67cb8664c47862d4eb24fdffbf48ad95c059cca
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-01-22 02:05:45 +0300

    insert data into DB example

--- src/JdbcTest.java
+++ src/JdbcTest.java
@@ -7,6 +7,10 @@ public class JdbcTest {
     private static final String PASSWORD = "student";
 
     private static final String SELECT_FROM_EMPLOYEES = "select * from employees";
+    private static final String INSERT_INTO_EMPLOYEES = "insert into employees (last_name, first_name, " +
+            "email, department, salary) values ('Wright', 'Eric', 'eric.wright@foo.com', 'HR', 33000.00)";
+    private static final String SELECT_FROM_EMPLOYEES_WHERE = "select last_name, first_name, email, " +
+            "department from employees where last_name = 'Wright' and first_name = 'Eric'";
 
     public static void main(String[] args) {
 
@@ -15,11 +19,14 @@ public class JdbcTest {
 
             System.out.println("Database connection successful!\n");
 
-            try (ResultSet myRs = myStmt.executeQuery(SELECT_FROM_EMPLOYEES)) {
+            myStmt.executeUpdate(INSERT_INTO_EMPLOYEES);
+
+            try (ResultSet myRs = myStmt.executeQuery(SELECT_FROM_EMPLOYEES_WHERE)) {
                 while (myRs.next()) {
                     System.out.println(myRs.getString("last_name") + ", "
                             + myRs.getString("first_name") + ", "
-                            + myRs.getString("email"));
+                            + myRs.getString("email") + ", "
+                            + myRs.getString("department"));
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
